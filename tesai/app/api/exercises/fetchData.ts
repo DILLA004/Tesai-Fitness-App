98bcdc7f569f8d9e4c75cca387c41aadbf48f684
Danylo Cherniavskyi <126616785+DILLA004@users.noreply.github.com>
2024-06-17 14:54:05 +0300

    Update fetchData.ts

--- tesai/app/api/exercises/fetchData.ts
+++ tesai/app/api/exercises/fetchData.ts
@@ -9,7 +9,7 @@ export const options = {
 export const youtubeOptions = {
     method: 'GET',
     headers: {
-        'x-rapidapi-key': 'b519009b3emshcd69ab56b32a722p14ed45jsndff6cefbb755',
+        'x-rapidapi-key': '11e348c08bmsh112bc8ed88737e6p1c3b01jsna2d4d3621642',
         'x-rapidapi-host': 'youtube-search-and-download.p.rapidapi.com'
     }
 };
